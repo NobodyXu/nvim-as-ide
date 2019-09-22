e293655f1046949bd52bec94d41c2ab5e6b612e2
nobodyxu <nobodyxu@lolol.me>
2019-09-23 00:30:24 +1000

    Fix install.sh: mkdir for init.vim before sym-linking it

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plugvim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+mkdir -p ~/.config/nvim
 ln -s `pwd`/init.vim ~/.config/nvim/init.vim
