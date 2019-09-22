26bbe05b3fca6b49e03c805f56ca07d3d57d3769
nobodyxu <nobodyxu@lolol.me>
2019-09-23 00:33:39 +1000

    Fix typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -fLo ~/.local/share/nvim/site/autoload/plugvim --create-dirs \
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim
