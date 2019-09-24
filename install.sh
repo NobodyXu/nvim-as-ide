e77b937f87cf8672bc7185b7c872a6317d38ba61
nobodyxu <nobodyxu@lolol.me>
2019-09-24 15:23:07 +1000

    Update install.sh: Backup init.vim file and then overwrite it

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim
-ln -s `pwd`/init.vim ~/.config/nvim/init.vim
+
+# Backup previous init.vim
+config_file=~/.config/nvim/init.vim
+mv $config_file ${config_file}.bk.`date -u | sed 's/ /_/g'`
+ln -s `pwd`/init.vim $config_file
 
 nvim -c ":PlugInstall"
