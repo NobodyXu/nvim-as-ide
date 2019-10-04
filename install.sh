233db9bd2170c4a80cd2fb9bdc9d6384ba3db5bf
nobodyxu <nobodyxu@lolol.me>
2019-10-04 20:23:10 +1000

    Symlink nvim config file to init.vim only if when the symlink does not already exist

--- install.sh
+++ install.sh
@@ -8,10 +8,12 @@ mkdir -p ~/.config/nvim
 # Backup previous init.vim
 config_file=~/.config/nvim/init.vim
 
-if [ -e $config_file ]; then
-    mv $config_file ${config_file}.bk.`date -u | sed 's/ /_/g'`
+if [ `realpath $config_file` != `realpath init.vim` ]; then
+    if [ -e $config_file ]; then
+        mv $config_file ${config_file}.bk.`date -u | sed 's/ /_/g'`
+    fi
+    
+    ln -s `pwd`/init.vim $config_file
 fi
-
-ln -s `pwd`/init.vim $config_file
 
 nvim -c ":PlugInstall"
